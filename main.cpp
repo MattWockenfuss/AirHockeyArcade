9a779652d663009ac92581d7c82b3754690f31b8
Grobe-Kartoffel <344briarwntling@gmail.com>
2026-03-05 17:02:06 -0500

    rotating fruit and a better distribution

--- main.cpp
+++ main.cpp
@@ -6,21 +6,27 @@
 
 struct Fruit{
 	sf::Texture tileSet;
-	int x;
+	int type;
+	int state;
+	double x;
 	double y;
 	int w;
 	int h;
+	double vx;
 	double vy;
 	int frame;
 	float time;
 	
-	Fruit(sf::Texture tileSet, int x, int w, int h){
+	Fruit(sf::Texture tileSet, int type, int state, int x, int y, int w, int h, double vx, double vy){
 		this->tileSet = tileSet;
+		this->type = type;
+		this->state = state;
 		this->x = x;
-		this->y = -h;
+		this->y = y;
 		this->w = w;
 		this->h = h;
-		this->vy = 0;
+		this->vx = vx;
+		this->vy = vy;
 		this->frame = 0;
 		this->time = 0.0;
 	}
@@ -30,6 +36,7 @@ struct Fruit{
 		for(int i = 0; i<n; i++){
 			vy += 0.1;
 			y += vy;
+			x += vx;
 		}
 		frame = (frame + n)%6;
 		time = std::remainder(time,0.075); // bring time back down while including any carry over between time steps
@@ -37,8 +44,8 @@ struct Fruit{
 	void draw(sf::RenderWindow* window, double screenRatio){
 		sf::Sprite sprite(tileSet);
 		sprite.setScale(sf::Vector2f(screenRatio,screenRatio));
-		sprite.setTextureRect(sf::IntRect( sf::Vector2(w*frame,0), sf::Vector2(w,h) )); // change this will change image we draw from tile set
-		sprite.setPosition(sf::Vector2f( ((19-(w/2))+(38*x))*screenRatio , y*screenRatio ));
+		sprite.setTextureRect(sf::IntRect( sf::Vector2(w*frame,h*state), sf::Vector2(w,h) )); // change this will change image we draw from tile set
+		sprite.setPosition(sf::Vector2f( ((27-(w/2))+x)*screenRatio , y*screenRatio ));
 		window->draw(sprite);
 	}
 };
@@ -77,6 +84,7 @@ int main()
 	bool guyRed = true;
 	bool guyCanChange = false;
 	bool guySwing = false;
+	bool cutFruit = false;
 	int guyMove = 0;
 	float guyStep = 0;
 	float guyTime = 0.0;
@@ -87,6 +95,7 @@ int main()
 	float fruitTime = 0;
 	int fruitType = 0;
 	int x = 0;
+	std::vector<int> fruitDist = {0,0,0,0,1,1,1,1,1,1,2,2,2,2,2,2,3,3,3,3,3,4,4,4,4,5,5,5,6,6,7};
 	
 	while ( window.isOpen() )
 	{
@@ -113,14 +122,14 @@ int main()
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left)){ // move left
 				if(guyX>0 && !guySwing && guyMove==0){
 					guyMove = -1;
-					guyFrame = 5;
+					guyFrame = 6;
 					guyStep = 0;
 				}
 			}
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right)){ // move right
 				if(guyX<7 && !guySwing && guyMove==0){
 					guyMove = 1;
-					guyFrame = 4;
+					guyFrame = 5;
 					guyStep = 0;
 				}
 			}
@@ -139,10 +148,38 @@ int main()
 			int n = round(guyTime/0.075f); // change frames every 0.075s (also account for lag that may cause a frame to be longer than 0.075s
 			guyTime = std::remainder(guyTime,0.075f); // reset guyTime while retaining any overflow
 			guyFrame += n;
-			if(guyFrame >= 4){ // stop animation
+			if(guyFrame==2 && !cutFruit){
+				int i;
+				for(i = 0; i<fruits.size(); i++){
+					if(fruits[i]->y+fruits[i]->h > 122 && fruits[i]->y+fruits[i]->h <= 133 && fruits[i]->state==0){
+						cutFruit = true;
+						break;
+					}
+				}
+				if(cutFruit){
+					int type = fruits[i]->type;
+					int x = fruits[i]->x;
+					int y = fruits[i]->y;
+					int w = fruits[i]->w;
+					int h = fruits[i]->h;
+					if(type==0){ // watermellon width changes when cut, other fruits do not
+						fruits.push_back( new Fruit(fruitTexs[type],type,1,x-w/2,y,16,h,-0.5,-1) );
+						fruits.push_back( new Fruit(fruitTexs[type],type,2,x+w/2,y,16,h,0.5,-1) );
+					} else {
+						fruits.push_back( new Fruit(fruitTexs[type],type,1,x-w/2,y,w,h,-0.5,-1) );
+						fruits.push_back( new Fruit(fruitTexs[type],type,2,x+w/2,y,w,h,0.5,-1) );
+					}
+					fruits[i]->y = 200*screenRatio;
+				}
+			}
+			if(guyFrame >= 4 + cutFruit?1:0){ // stop animation
 				guySwing = false;
 				guyFacing = (guyFacing+1)%2; // will alternate between o and 1
 				guyFrame = 0;
+				cutFruit = false;
+			}
+			if(guyFrame==3 && cutFruit){ // did we cut fruit?
+				guyFrame = 4;
 			}
 		}
 		if(guyMove!=0){
@@ -162,26 +199,28 @@ int main()
 		if(fruitTime <= 0){
 			fruitType = rand()%4;
 			if(fruitType==0){
-				fruits.push_back( new Fruit(fruitTexs[fruitType],x,20,16) ); // drop mellon
+				fruits.push_back( new Fruit(fruitTexs[fruitType],fruitType,0,x*38,-16,20,16,0,0) ); // drop mellon
 			}
 			else{
-				fruits.push_back( new Fruit(fruitTexs[fruitType],x,8,8) ); // drop other fruit (they are all the same size)
+				fruits.push_back( new Fruit(fruitTexs[fruitType],fruitType,0,x*38,-8,8,8,0,0) ); // drop other fruit (they are all the same size)
 			}
 			
 			fruitTime = rand()%2; // choose whether the new fruit will fall to the left or to the right
 			if(fruitTime==0){ // move the fruit a ranom number of columns (stay close enough that it is reachable within time)
-				fruitTime = rand()&10; // random column movement between 0 and 9
+				fruitTime = fruitDist[ rand()%( fruitDist.size() ) ];
+				//fruitTime = rand()&10; // random column movement between 0 and 9
 				x += fruitTime;
 				if(x>7)
 					x = 7;
 			}
 			else{ // move in the other direction
-				fruitTime = rand()&10; // random column movement between 0 and 9
+				fruitTime = fruitDist[ rand()%( fruitDist.size() ) ];
+				//fruitTime = rand()&10; // random column movement between 0 and 9
 				x -= fruitTime;
 				if(x<0)
 					x = 0;
 			}
-			fruitTime = (fruitTime+1)*0.3; // turn random column movement into random time spacing between fruits of 0.3s (same column) to 3.0s (any column plus a pause)
+			fruitTime = (fruitTime+1)*0.4; // turn random column movement into random time spacing between fruits of 0.4s (same column) to 4.0s (any column plus a pause)
 		}
 		fruitTime -= dt;
 		// move fruit
@@ -201,12 +240,12 @@ int main()
 		window.clear();
 		// background
 		window.draw(back);
-		// guy
-		window.draw(guy);
 		// fruit
 		for(int i = 0; i<fruits.size(); i++){
 			fruits[i]->draw(&window, screenRatio);
 		}
+		// guy
+		window.draw(guy);
 		window.display();
 	}
 	return 0;
