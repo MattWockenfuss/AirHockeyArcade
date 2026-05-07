47f26effc7a3c0a79b44458caeec5e0070593710
Matt Wockenfuss <223121734+MattWockenfuss@users.noreply.github.com>
2026-05-07 13:16:24 -0400

    added comments

--- run-pi.sh
+++ run-pi.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+
+# We are using OpenBox, a window manager, and X11
+# to run a tiny new 'session' that doesnt have a desktop environment
+# perfect for us as we are trying to make it very lightweight and fullscreened
+# without true full screened
+
+
+#to run the game
+# first build using 'sh build-pi.sh'
+
+# next run using 'startx ./run-linux.sh' 
+
 xset -dpms
 xset s off
 xset s noblank
